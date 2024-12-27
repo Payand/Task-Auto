2e59aa1bdaee4fbd1bef10eb7f56c9fe4ed41f8e
a.shooshtari <shooshtariali05@gmail.com>
2024-12-27 17:56:47 +0330

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/yWny4P2Spp

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'auto-abzar',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/auto-abzar',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
